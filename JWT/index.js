3f35cf603400749092635741c90827111d17b7ca
Yash-Bohra <95557948+Yash-Bohra@users.noreply.github.com>
2024-09-20 22:52:09 +0530

    Update index.js

--- JWT/index.js
+++ JWT/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
-const JWT_SECRET = "iamYASHBOHRA"
+const JWT_SECRET = ""
 const app = express()
 users=[]
 app.use(express.json())
